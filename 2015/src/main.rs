b29a3057f57c75c7c81538097f4330a985880b5f
Mark Triggs <mark@dishevelled.net>
2018-04-16 16:14:13 +1000

    Here we go!

---
+++ 2015/src/main.rs
@@ -0,0 +1,24 @@
+// (cd ../ && cargo build && scp target/debug/adventofcode2016 mozart:tmp/ && ssh mozart '(cd /home/mst/tmp; RUST_BACKTRACE=1 ~/tmp/adventofcode2016)')
+
+
+// use regex::Regex;
+// use std::cell::RefCell;
+// use std::collections::HashMap;
+// use std::collections::HashSet;
+// use std::fs::File;
+// use std::io::{BufRead, BufReader};
+// use std::io::{BufReader, BufRead};
+// use std::io::{BufReader, Read};
+// use std::iter::FromIterator;
+// use std::rc::Rc;
+//
+// extern crate regex;
+
+/*
+
+*/
+
+///////////////// Day 1
+
+fn main() {
+}
