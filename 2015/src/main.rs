1152d4ce356a26866ca97dcb5d2663e42ba556bf
Mark Triggs <mark@dishevelled.net>
2018-04-16 16:54:47 +1000

    Day 1

--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -20,5 +20,34 @@
 
 ///////////////// Day 1
 
+use std::fs::File;
+use std::io::Read;
+use std::io::BufReader;
+
+fn day1() {
+    let f = File::open("advent-files/day1-input.txt").expect("open file");
+    let mut br = BufReader::new(f);
+
+    let mut input = String::new();
+    br.read_to_string(&mut input).unwrap();
+
+    let result = input.trim().chars().enumerate().fold(0, |floor, (idx, ch)| {
+        let new_floor = match ch {
+            '(' => { floor + 1 }
+            ')' => { floor - 1 }
+            _ => { panic!("Invalid input: {}", ch) }
+        };
+
+        if floor == 0 && new_floor == -1 {
+            println!("Entered the basement at position: {}", idx + 1);
+        }
+
+        new_floor
+    });
+
+    println!("Final floor: {}", result);
+}
+
 fn main() {
+    day1();
 }
