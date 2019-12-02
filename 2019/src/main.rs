bd6878f62563da0c7e58d6139cf7661cc790e4b8
Mark Triggs <mark@dishevelled.net>
2019-12-02 19:09:05 +1100

    day 2

--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -1,5 +1,6 @@
 // (cd ../; cargo run --release)
 
+#![allow(unused_imports)]
 #![allow(unused_parens)]
 #![allow(dead_code)]
 
@@ -91,11 +92,82 @@ mod day1 {
 
         dbg!(result);
     }
+}
 
 
+mod day2 {
+    use crate::shared::*;
+
+    fn intcode_eval(code: &str, noun: i64, verb: i64) -> i64 {
+        let mut state: Vec<i64> = code.split(",").map(|s| s.parse().unwrap()).collect();
+        let mut pc = 0;
+
+        state[1] = noun;
+        state[2] = verb;
+
+        while state[pc] != 99 {
+            let op1_addr = state[pc + 1] as usize;
+            let op2_addr = state[pc + 2] as usize;
+            let target_addr = state[pc + 3] as usize;
+
+            match state[pc] {
+                1 => { state[target_addr] = state[op1_addr] + state[op2_addr]; },
+                2 => { state[target_addr] = state[op1_addr] * state[op2_addr]; },
+                _ => panic!("invalid input"),
+            };
+
+            pc += 4;
+        }
+
+        state[0]
+    }
+
+    pub fn part1() {
+        println!("{}", intcode_eval("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,1,6,27,31,2,31,\
+                                     13,35,1,9,35,39,2,39,13,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55,9,59,1,59,5,\
+                                     63,1,6,63,67,1,13,67,71,2,71,10,75,1,6,75,79,1,79,10,83,1,5,83,87,2,10,87,91,\
+                                     1,6,91,95,1,9,95,99,1,99,9,103,2,103,10,107,1,5,107,111,1,9,111,115,2,13,115,\
+                                     119,1,119,10,123,1,123,10,127,2,127,10,131,1,5,131,135,1,10,135,139,1,139,2,\
+                                     143,1,6,143,0,99,2,14,0,0",
+                                    12, 2));
+    }
+
+    pub fn part2() {
+        for noun in 0..100 {
+            for verb in 0..100 {
+                let result = intcode_eval("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,1,6,27,31,2,31,\
+                                           13,35,1,9,35,39,2,39,13,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55,9,59,1,59,5,\
+                                           63,1,6,63,67,1,13,67,71,2,71,10,75,1,6,75,79,1,79,10,83,1,5,83,87,2,10,87,91,\
+                                           1,6,91,95,1,9,95,99,1,99,9,103,2,103,10,107,1,5,107,111,1,9,111,115,2,13,115,\
+                                           119,1,119,10,123,1,123,10,127,2,127,10,131,1,5,131,135,1,10,135,139,1,139,2,\
+                                           143,1,6,143,0,99,2,14,0,0",
+                                          noun, verb);
+
+                if result == 19690720 {
+                    dbg!(100 * noun + verb);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
+
+mod day_n {
+    use crate::shared::*;
+
+    pub fn part1() {}
+    pub fn part2() {}
 }
 
 fn main() {
-    day1::part1();
-    day1::part2();
+    if false {
+        day1::part1();
+        day1::part2();
+
+        day2::part1();
+        day2::part2();
+    }
 }
+
