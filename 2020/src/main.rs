6a640c1a410e4200bf8f4a62b7fa9f1c349b12e2
Mark Triggs <mark@dishevelled.net>
2020-12-01 14:36:24 +1100

    Ready spaghetti

---
+++ 2020/src/main.rs
@@ -0,0 +1,118 @@
+// (cd ../; cargo run --release)
+
+#![allow(unused_imports)]
+#![allow(unused_parens)]
+#![allow(dead_code)]
+
+extern crate lazy_static;
+extern crate regex;
+
+
+mod shared {
+    pub use regex::Regex;
+
+    // pub use intcode::{self, IntCode};
+    pub use std::cell::RefCell;
+    pub use std::cmp::{self, Ordering};
+    pub use std::collections::BTreeSet;
+    pub use std::collections::BTreeMap;
+    pub use std::collections::HashMap;
+    pub use std::collections::HashSet;
+    pub use std::collections::LinkedList;
+    pub use std::collections::VecDeque;
+    pub use std::fmt::{self, Display};
+    pub use std::fs::{self, File};
+    pub use std::io::{self, BufRead, BufReader, Write, Read};
+    pub use std::iter::FromIterator;
+    pub use std::str;
+    pub use std::convert::TryInto;
+
+    pub const ALPHABET: &str = "abcdefghijlkmnopqrstuvwxyz";
+    pub const ALPHABET_UPPER: &str = "ABCDEFGHIJLKMNOPQRSTUVWXYZ";
+
+    fn regex_examples() {
+        let simple_match = Regex::new(r"s.mple match").unwrap();
+        if simple_match.is_match("simple match") {
+            println!("Matched!");
+        }
+
+        let extract_numbers = Regex::new(r"dimensions (\d+)x(\d+) left (\d+) top (\d+)").unwrap();
+        for cap in extract_numbers.captures_iter("dimensions 640x480 left 100 top 100") {
+            println!(
+                "width: {}; height: {}; left: {}; top: {}.  Full line: {}",
+                &cap[1], &cap[2], &cap[3], &cap[4], &cap[0]
+            );
+        }
+
+        let replace_regex = Regex::new(r"h.llo").unwrap();
+        println!(
+            "{}",
+            replace_regex.replace_all("hello hello hello", "goodbye")
+        );
+    }
+
+    pub fn read_file(file: &str) -> String {
+        fs::read_to_string(file).unwrap().trim().to_owned()
+    }
+
+    // No trim!
+    pub fn read_file_raw(file: &str) -> String {
+        fs::read_to_string(file).unwrap().to_owned()
+    }
+
+    pub fn input_lines(file: &str) -> impl Iterator<Item = String> {
+        let f = File::open(file).unwrap_or_else(|_| panic!("Failed to open input file: {}", &file));
+        BufReader::new(f).lines().map(Result::unwrap)
+    }
+
+    pub fn sample_input(input: &str) -> Vec<String> {
+        input.trim().split('\n').map(str::to_owned).collect()
+    }
+
+    pub fn permutations<T>(inputs: Vec<T>) -> Vec<Vec<T>>
+    where
+        T: Clone + Copy + std::fmt::Debug,
+    {
+        if inputs.is_empty() {
+            vec![Vec::new()]
+        } else {
+            let elt = inputs.get(0).unwrap();
+            let subperms = permutations(inputs.iter().skip(1).cloned().collect());
+
+            subperms
+                .iter()
+                .flat_map(|subperm: &Vec<T>| {
+                    (0..=subperm.len()).map(move |idx| {
+                        let mut r = subperm.clone();
+                        r.insert(idx, *elt);
+                        r
+                    })
+                })
+                .collect()
+        }
+    }
+}
+
+mod day1 {
+    use crate::shared::*;
+
+    pub fn part1() {
+    }
+
+
+    pub fn part2() {
+    }
+}
+
+
+mod day_n {
+    use crate::shared::*;
+
+    pub fn part1() {}
+    pub fn part2() {}
+}
+
+fn main() {
+    day1::part1();
+    day1::part2();
+}
